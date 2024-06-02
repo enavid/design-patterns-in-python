753156c3364b9321eb10c4d6639582cd6425cf04
enavid <navidsadeghi2@outlook.com>
2024-06-02 21:08:03 +0330

    update

---
+++ 1. SOLID Principles/single-responsibility-principle.py
@@ -0,0 +1,21 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count = self.count + 1
+        self.entries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+
+
+j = Journal()
+j.add_entry('I cried today.')
+j.add_entry('I ate a bug.')
+print(f'Journal entries: {j}')
